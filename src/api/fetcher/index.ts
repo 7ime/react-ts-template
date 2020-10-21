7796350bf5ec0b7c7b96201478b923cddf755247
7ime <lopatsin1990@gmail.com>
2020-10-21 12:04:07 +0300

    refactoring fetch

--- src/api/fetcher/index.ts
+++ src/api/fetcher/index.ts
@@ -1,46 +1,63 @@
-import axios, {AxiosInstance} from 'axios'
+import axios, {AxiosInstance, AxiosRequestConfig} from 'axios'
 import {IFetcher} from './model'
 import MockAdapter from 'axios-mock-adapter'
+import {EHTTPMethods} from '@constants/shared'
 
 export const axiosMockAdapter = new MockAdapter(axios, {onNoMatch: 'passthrough'})
 
 class Fetcher implements IFetcher{
-    private axios: AxiosInstance
+    private readonly axios: AxiosInstance
+
+    private headers: Record<string, string> = {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'X-Requested-With': 'XMLHttpRequest'
+    }
 
     constructor() {
         this.axios = axios.create({
             withCredentials: false,
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-                'X-Requested-With': 'XMLHttpRequest'
-            },
             responseType: 'json'
         })
     }
 
-    public get = async(url: string, body: any = {}) => {
-        const result = await this.axios.get(url, body)
-
-        return result.data
+    addHeader(name: string, value: string): void {
+        this.headers[name] = value
     }
 
-    public post = async(url: string, body: any = {}) => {
-        const result = await this.axios.post(url, body)
-
-        return result.data
+    deleteHeader(name: string): void {
+        delete this.headers[name]
     }
 
-    public put = async(url: string, body: any = {}) => {
-        const result = await this.axios.put(url, body)
-
-        return result.data
+    get = async<T>(url: string, body: any = {}): Promise<T> => {
+        return this.invokeRequest(this.shapeRequestConfig(url, body, EHTTPMethods.get))
     }
 
-    public delete = async(url: string, body: any = {}) => {
-        const result = await this.axios.delete(url, body)
+    post = async<T>(url: string, body: any = {}): Promise<T> => {
+        return this.invokeRequest(this.shapeRequestConfig(url, body, EHTTPMethods.post))
+    }
 
-        return result.data
+    put = async<T>(url: string, body: any = {}): Promise<T> => {
+        return this.invokeRequest(this.shapeRequestConfig(url, body, EHTTPMethods.put))
+    }
+
+    delete = async<T>(url: string, body: any = {}): Promise<T> => {
+        return this.invokeRequest(this.shapeRequestConfig(url, body, EHTTPMethods.delete))
+    }
+
+    private shapeRequestConfig(url: string, body: any, method: EHTTPMethods): AxiosRequestConfig {
+        return {
+            url,
+            method,
+            headers: this.headers,
+            data: body
+        }
+    }
+
+    private invokeRequest = async<T>(config: AxiosRequestConfig): Promise<T> => {
+        const response = await this.axios(config)
+
+        return response.data
     }
 }
 
--- src/constants/shared.ts
+++ src/constants/shared.ts
@@ -8,3 +8,10 @@ export enum EPosition {
     AFTER = 'AFTER',
     BEFORE = 'BEFORE'
 }
+
+export enum EHTTPMethods {
+    get = 'get',
+    post = 'post',
+    put = 'put',
+    delete = 'delete'
+}
