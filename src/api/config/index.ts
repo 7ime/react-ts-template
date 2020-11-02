d3fcef0da43325364678486cad007013ef44053c
7ime <lopatsin1990@gmail.com>
2020-11-02 16:56:25 +0300

    package.json added homepage-storybook

--- src/api/config/index.ts
+++ src/api/config/index.ts
@@ -1,7 +1,5 @@
-import {environment} from '@toolbox/environment'
-
 export const configuration = Object.freeze(
     {
-        remoteApi: environment.production ? '/api/v1' : 'https://jsonplaceholder.typicode.com'
+        remoteApi: 'https://jsonplaceholder.typicode.com'
     }
 )
