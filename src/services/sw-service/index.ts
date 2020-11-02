07e80724a04b10db55666215c07478a6efdd0a45
7ime <lopatsin1990@gmail.com>
2020-11-02 16:07:59 +0300

    change register service worker in relation to environment

--- src/services/sw-service/index.ts
+++ src/services/sw-service/index.ts
@@ -1,9 +1,10 @@
 import runtime from 'serviceworker-webpack-plugin/lib/runtime'
 import {ISwService} from '@services/sw-service/model'
+import {environment} from '@toolbox/environment'
 
 export default class SwService implements ISwService {
     register(): Promise<ServiceWorkerRegistration> {
-        return runtime.register()
+        return environment.production ? navigator.serviceWorker.register('sw.js'): runtime.register()
     }
 
     unregister(worker: ServiceWorkerRegistration): Promise<boolean> {
