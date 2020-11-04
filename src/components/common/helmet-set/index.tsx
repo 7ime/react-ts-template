3e998dc9b8f5d8d156d854d1d433d4d2d900ec87
7ime <lopatsin1990@gmail.com>
2020-11-04 08:42:39 +0300

    refactoring helmet set

--- src/components/common/helmet-set/index.tsx
+++ src/components/common/helmet-set/index.tsx
@@ -3,16 +3,9 @@ import {Helmet} from 'react-helmet'
 
 const HelmetSet: React.FC = () => {
     return (
-        <Helmet
-            link={[
-                {
-                    rel: 'shortcut icon',
-                    type: 'image/x-icon',
-                    href: require('@images/favicon/apple-icon-60x60.png'),
-                    sizes: '60x60'
-                }
-            ]}
-        />
+        <Helmet>
+            <link rel="shortcut icon" href={require('@images/favicon/apple-icon-60x60.png')} sizes='60x60' type='image/x-icon' />
+        </Helmet>
     )
 }
 
